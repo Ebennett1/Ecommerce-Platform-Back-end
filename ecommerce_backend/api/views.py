f4b72419416fd23ef5725ac23d016c81955de1ea
Ebennett1 <evontebennett@gmail.com>
2024-05-26 22:09:42 -0700

    Small update

--- ecommerce_backend/api/views.py
+++ ecommerce_backend/api/views.py
@@ -75,6 +75,21 @@ class UpdateCartItem(generics.RetrieveUpdateDestroyAPIView):
     queryset = CartItem.objects.all()
     serializer_class = CartItemSerializer
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+
+        # Validate that quantity is not negative
+        quantity = request.data.get('quantity')
+        if quantity is not None and int(quantity) < 0:
+            return Response({"detail": "Quantity cannot be negative"}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return Response(serializer.data)
+
 class OrderList(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
