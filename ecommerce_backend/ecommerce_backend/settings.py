df0bb0d58229f76dca131cdcb10eaa4c314231ee
Ebennett1 <evontebennett@gmail.com>
2024-06-08 22:02:21 -0700

    Update database settings for local and Heroku environments

--- ecommerce_backend/ecommerce_backend/settings.py
+++ ecommerce_backend/ecommerce_backend/settings.py
@@ -11,31 +11,30 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_qev&+^w#z^*nx1@_-vgg04iyxs9c+1g=hxd0a5m3-det%%!vc'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-_qev&+^w#z^*nx1@_-vgg04iyxs9c+1g=hxd0a5m3-det%%!vc')
 
-STRIPE_SECRET_KEY = 'sk_test_51PMLrxL6Ra3fxI3YgIdevVqeIOHlljiY1afSCfx8Rh4RR3Q67mUaN0V7f9pqrz2AP1t2P64onjt5cRcRp4PuqNbI00ouIJ65EQ'
-
-UNSPLASH_SECRET_KEY = 'W6QdnPQaUc4sWRhhdos4jN6ZOwAHfHeCsROn4xrB0jQ'
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', 'sk_test_51PMLrxL6Ra3fxI3YgIdevVqeIOHlljiY1afSCfx8Rh4RR3Q67mUaN0V7f9pqrz2AP1t2P64onjt5cRcRp4PuqNbI00ouIJ65EQ')
+UNSPLASH_SECRET_KEY = os.environ.get('UNSPLASH_SECRET_KEY', 'W6QdnPQaUc4sWRhhdos4jN6ZOwAHfHeCsROn4xrB0jQ')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['desktop-1foc6dm','localhost', '127.0.0.1' ]
-
+ALLOWED_HOSTS = ['your-heroku-app.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
 INSTALLED_APPS = [
-   'django.contrib.sites',
+    'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -55,17 +54,14 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 AUTHENTICATION_BACKENDS = (
-    
     'allauth.account.auth_backends.AuthenticationBackend',
-    
 )
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -75,7 +71,6 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'allauth.account.middleware.AccountMiddleware',
-    
 ]
 
 ROOT_URLCONF = 'ecommerce_backend.urls'
@@ -98,19 +93,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ecommerce_backend.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ecommerce_site',
-        'USER': 'ebennett',
-        'PASSWORD': 'sasuke14',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default='postgres://ebennett:sasuke14@localhost:5432/ecommerce_site',
+        conn_max_age=600,
+        ssl_require=os.getenv('DATABASE_URL') is not None
+    )
 }
 
 from datetime import timedelta
@@ -122,7 +113,6 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': True,
 }
 
-
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
@@ -133,7 +123,7 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-     "http://localhost:3000",
+    "http://localhost:3000",
     "http://127.0.0.1:3000"
 ]
 
@@ -162,7 +152,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -174,19 +163,14 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-import os
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
@@ -195,3 +179,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Heroku settings
+import django_heroku
+django_heroku.settings(locals())
