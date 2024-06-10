6e7b1d7a074c224990fe3b4885487567bf6e72db
Ebennett1 <evontebennett@gmail.com>
2024-06-10 00:50:08 -0700

    Update settings module path in asgi.py, wsgi.py, manage.py, and Procfile

--- ecommerce_backend/manage.py
+++ ecommerce_backend/manage.py
@@ -5,7 +5,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_backend.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_backend.ecommerce_backend.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,3 +18,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
